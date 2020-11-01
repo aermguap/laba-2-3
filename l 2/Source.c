0f9703cb26a89e11172b9c6bb6cd0607d16e1875
Artem E 2021 <aermilov756@gmail.com>
2020-11-01 20:24:31 +0300

    first

---
+++ l 2/Source.c
@@ -0,0 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#define Check_Symbol ( ( (s[i] > 64) & (s[i] < 91) ) || ( (s[i] > 96) & (s[i] < 123) ) || ( (s[i] > 127) & (s[i] < 176) ) || ( (s[i] > 223) & (s[i] < 240) ) )
+
+
+void print(unsigned char* m, int a, int b)
+{
+	for (int i = a; i <= b; i++)
+	{
+		printf("%c", m[i]);
+	}
+
+	printf("\n");
+}
+
+
+int main()
+{
+	unsigned char s[100];
+
+	printf("Enter a string \n");
+
+	int a[50][3] = { 0 };
+
+
+	gets(s);
+
+	int i = 0;	int x = 0; 	int j = 0;	int min = 0;	int t = 0;	int k = 0;	int minInd = 0;
+
+
+	while (s[i] != '\0')
+	{
+		x++;
+
+		i++;
+	}
+
+
+	for (int i = 0; i <= x - 1; i++)
+	{
+		if (Check_Symbol & (t == 0))
+		{
+			a[j][t] = i;
+
+			t = 1;
+
+		}
+		if ((!(Check_Symbol)) & (t == 1))
+		{
+
+			a[j][t] = i - 1;
+
+			j++;
+
+			t = 0;
+
+		}
+		if ((s[i + 1] == '\0') & (t == 1))
+		{
+			a[j][1] = i;
+
+		}
+
+	}
+
+	j = 1;
+	while (a[j][1] != 0)
+	{
+		k++;
+
+		j++;
+	}
+	min = 100;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("\n");
+
+	}
+
+	for (j = 0; j <= k; j++)
+	{
+		if (a[j][1] - a[j][0] + 1 < min)
+		{
+			min = a[j][1] - a[j][0] + 1;
+
+			minInd = j;
+		}
+	}
+	//сравнение каждого слова с минимальной длиной
+
+
+	printf("\nShortest word: \n");
+
+	print(s, a[minInd][0], a[minInd][1]);
+	//вывод повтор€ющихс€ слов с 1 в 3 столбце
+
+	return 0;
+}
---
+++ l 3/Source.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+float funct(float num1, float num2)  // определение функции
+{
+	if (num2 == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		{
+			float x;
+
+			x = num1;
+			if (num2 < 0) {
+				for (int i = -1; i > num2; i--)
+				{
+					num1 = x * num1;
+				}
+				return 1 / num1;
+			}
+
+			else {
+				for (int i = 1; i < num2; i++)
+				{
+					num1 = x * num1;
+				}
+
+				return num1;
+			}
+		}
+	}
+}
+
+int main()
+{
+	float x;
+	float y;
+
+	printf("enter the number and the degree to which you want to raise the number: ");
+	if (scanf("%f", &x) != 1)
+	{
+		printf("Input Error");
+
+		return 0;
+	}
+	if (scanf("%f", &y) != 1)
+	{
+		printf("Input Error");
+		return 0;
+	}
+	printf("result: %f\n", funct(x, y)); // вызов функции
+
+	return 0;
+}
+
