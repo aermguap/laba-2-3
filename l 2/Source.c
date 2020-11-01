484b2e11fae90e337dcbe264bb04c28014947521
aermguap <71142772+aermguap@users.noreply.github.com>
2020-11-01 21:50:38 +0300

    Update Source.c

--- l 2/Source.c
+++ l 2/Source.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-#define Check_Symbol ( ( (s[i] > 64) & (s[i] < 91) ) || ( (s[i] > 96) & (s[i] < 123) ) || ( (s[i] > 127) & (s[i] < 176) ) || ( (s[i] > 223) & (s[i] < 240) ) )
+#define Check_Symbol ( ( (s[i] > 64) && (s[i] < 91) ) || ( (s[i] > 96) && (s[i] < 123) ) || ( (s[i] > 127) && (s[i] < 176) ) || ( (s[i] > 223) && (s[i] < 240) ) )
 //print the shortest word in a line
 
 void print(unsigned char* m, int a, int b)
