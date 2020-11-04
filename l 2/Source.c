06a35969057560787318856b577552db30f72153
aermguap <71142772+aermguap@users.noreply.github.com>
2020-11-04 15:23:30 +0300

    Update Source.c

--- l 2/Source.c
+++ l 2/Source.c
@@ -1,95 +1,54 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define Check_Symbol ( ( (s[i] > 64) && (s[i] < 91) ) || ( (s[i] > 96) && (s[i] < 123) ) || ( (s[i] > 127) && (s[i] < 176) ) || ( (s[i] > 223) && (s[i] < 240) ) )
 //print the shortest word in a line
-void print(unsigned char* m, int a, int b)
-{
+void print(unsigned char* m, int a, int b){
 	for (int i = a; i <= b; i++)
 	{
-		printf("%c", m[i]);
+	printf("%c", m[i]);
 	}
-
 	printf("\n");
 }
 
 
-int main()
-{
+int main(){
 	unsigned char s[100];
-
 	printf("Enter a string \n");
-
 	int a[50][3] = { 0 };
-
-
 	gets(s);
-
 	int i = 0;	int x = 0; 	int j = 0;	int min = 0;	int t = 0;	int k = 0;	int minInd = 0;
-
-
-	while (s[i] != '\0')
-	{
+	while (s[i] != '\0'){
 		x++;
-
 		i++;
 	}
-
-
-	for (int i = 0; i <= x - 1; i++)
-	{
-		if (Check_Symbol & (t == 0))
-		{
+	for (int i = 0; i <= x - 1; i++){
+		if (((s[i] > 64) && (s[i] < 91)) || ((s[i] > 96) && (s[i] < 123)) || ((s[i] > 127) && (s[i] < 176)) || ((s[i] > 223) && (s[i] < 240)) & (t == 0)){
 			a[j][t] = i;
-
 			t = 1;
-
 		}
-		if ((!(Check_Symbol)) & (t == 1))
-		{
-
+		if ((!(((s[i] > 64) && (s[i] < 91)) || ((s[i] > 96) && (s[i] < 123)) || ((s[i] > 127) && (s[i] < 176)) || ((s[i] > 223) && (s[i] < 240)))) & (t == 1)){
 			a[j][t] = i - 1;
-
 			j++;
-
 			t = 0;
-
 		}
-		if ((s[i + 1] == '\0') & (t == 1))
-		{
+		if ((s[i + 1] == '\0') & (t == 1)){
 			a[j][1] = i;
-
 		}
-
 	}
-
 	j = 1;
-	while (a[j][1] != 0)
-	{
+	while (a[j][1] != 0){
 		k++;
-
 		j++;
 	}
 	min = 100;
-
-	for (i = 0; i < 10; i++)
-	{
+	for (i = 0; i < 10; i++){
 		printf("\n");
-
 	}
-
-	for (j = 0; j <= k; j++)
-	{
-		if (a[j][1] - a[j][0] + 1 < min)
-		{
+	for (j = 0; j <= k; j++){
+		if (a[j][1] - a[j][0] + 1 < min){
 			min = a[j][1] - a[j][0] + 1;
-
 			minInd = j;
 		}
 	}
-	
-
 	printf("\nShortest word: \n");
-
 	print(s, a[minInd][0], a[minInd][1]);
 	return 0;
-}
